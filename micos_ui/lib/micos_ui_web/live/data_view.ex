2721d81e5d050508160cc362a06064007e5d6b17
Sven Bohm <bohms@msu.edu>
2019-04-25 11:36:28 -0400

    update timer every second

--- micos_ui/lib/micos_ui_web/live/data_view.ex
+++ micos_ui/lib/micos_ui_web/live/data_view.ex
@@ -2,17 +2,17 @@ defmodule MicosUiWeb.DataView do
   use Phoenix.LiveView
 
   def render(assigns) do
-    ~L"""
-    <div class="">
-      <div>
-        <%= @datetime%>
-      </div>
-    </div>
-    """
+    MicosUiWeb.PageView.render("data_view.html", assigns)
   end
 
   def mount(_session, socket) do
+    Process.send_after(self(), :tick, 1_000)
     {:ok, assign(socket, datetime: DateTime.utc_now )}
+  end
+
+  def handle_info(:tick, socket) do
+    Process.send_after(self(), :tick, 1_000)
+    {:noreply, assign(socket, datetime: DateTime.utc_now) }
   end
 
 end
