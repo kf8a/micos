ad2ebae7197d4c0748d55c27546de884bf65781d
sven bohm <bohms@ideomatic.io>
2019-05-02 22:44:04 +0100

    move the database info out

--- micos_ui/config/dev.exs
+++ micos_ui/config/dev.exs
@@ -68,7 +68,4 @@ config :phoenix, :stacktrace_depth, 20
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
 
-config :micos_ui, MicosUi.Repo,
-  database: "micosui_dev",
-  hostname: "localhost",
-  pool_size: 10
+# import_config "dev.secret.exs"
