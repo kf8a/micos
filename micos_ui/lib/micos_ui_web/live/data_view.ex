622650c729c150d51e92e05f5951a3072be2af4e
Sven Bohm <bohms@msu.edu>
2019-05-03 14:35:13 -0400

    round fluxes and r2

--- micos_ui/lib/micos_ui_web/live/data_view.ex
+++ micos_ui/lib/micos_ui_web/live/data_view.ex
@@ -16,15 +16,38 @@ defmodule MicosUiWeb.DataView do
     data = status[:data]
     plots = Samples.get_plots_for_select()
 
+    fluxes = round3(flux_to_map(status))
+
+    Endpoint.subscribe("data")
+
+    live = %{data: data, sampling: status[:sampling],
+            changeset: Samples.change_sample(%Sample{}), plots: plots}
+
+    {:ok, assign(socket, Map.merge(live, fluxes))}
+  end
+
+  def flux_to_map(status) do
     n2o_flux = status[:n2o_flux]
     co2_flux = status[:co2_flux]
     ch4_flux = status[:ch4_flux]
 
-    Endpoint.subscribe("data")
-    {:ok, assign(socket, datetime: DateTime.utc_now, data: data, sampling: status[:sampling],
-      n2o_flux: n2o_flux[:slope], n2o_r2: n2o_flux[:r2], co2_flux: co2_flux[:slope], co2_r2: co2_flux[:r2],
-      ch4_flux: ch4_flux[:slope], ch4_r2: ch4_flux[:r2],
-      changeset: Samples.change_sample(%Sample{}), plots: plots )}
+    %{n2o_flux: n2o_flux[:slope], n2o_r2: n2o_flux[:r2],
+      co2_flux: co2_flux[:slope], co2_r2: co2_flux[:r2],
+      ch4_flux: ch4_flux[:slope], ch4_r2: ch4_flux[:r2]}
+  end
+
+  def round3(fluxes) do
+    Enum.map(fluxes, fn(x) -> round_to_3(x) end)
+    |> Enum.into(%{})
+  end
+
+  def round_to_3({key, x}) do
+    case x do
+      nil ->
+        {key, x}
+      _ ->
+        {key, Float.round(x,3)}
+    end
   end
 
   def handle_event("sample", _value, socket) do
@@ -32,23 +55,18 @@ defmodule MicosUiWeb.DataView do
     status = MicosUi.Instrument.status()
     data = status[:data]
 
-    n2o_flux = status[:n2o_flux]
-    co2_flux = status[:co2_flux]
-    ch4_flux = status[:ch4_flux]
-
-    {:noreply, assign(socket, datetime: DateTime.utc_now, data: data, sampling: status[:sampling],
-      n2o_flux: n2o_flux[:slope], n2o_r2: n2o_flux[:r2], co2_flux: co2_flux[:slope], co2_r2: co2_flux[:r2],
-      ch4_flux: ch4_flux[:slope], ch4_r2: ch4_flux[:r2])}
+    live = %{data: data, sampling: status[:sampling]}
+    {:noreply, assign(socket, Map.merge(live, round3(flux_to_map(status)))) }
   end
 
   def handle_event("stop", _value, socket) do
     MicosUi.Instrument.stop()
     status = MicosUi.Instrument.status()
-    {:noreply, assign(socket, datetime: DateTime.utc_now, sampling: status[:sampling]) }
+    {:noreply, assign(socket, sampling: status[:sampling]) }
   end
 
   def handle_event("next", _value, socket) do
-    {:noreply, assign(socket, datetime: DateTime.utc_now, changeset: Samples.change_sample(%Sample{})) }
+    {:noreply, assign(socket, changeset: Samples.change_sample(%Sample{})) }
   end
 
   def handle_event("validate",  %{"sample" => params}, socket) do
@@ -66,7 +84,7 @@ defmodule MicosUiWeb.DataView do
 
   def handle_info(%Phoenix.Socket.Broadcast{event: "new", payload: payload, topic: "data"} = _event, %Phoenix.LiveView.Socket{assigns: assigns} = socket) do
     data = assigns[:data] ++ [payload]
-    {:noreply, assign(socket, datetime: DateTime.utc_now, data: data)}
+    {:noreply, assign(socket, data: data)}
   end
 
   def handle_info(%Phoenix.Socket.Broadcast{event: "flux", payload: %{ch4_flux: {:error}, co2_flux: {:error}, n2o_flux: {:error}} = payload, topic: "data"}, %Phoenix.LiveView.Socket{} = socket) do
@@ -79,10 +97,11 @@ defmodule MicosUiWeb.DataView do
     co2 = payload[:co2_flux]
     ch4 = payload[:ch4_flux]
 
-    {:noreply, assign(socket,
-      datetime: DateTime.utc_now, n2o_flux: n2o[:slope],
-      n2o_r2: n2o[:r2], co2_flux: co2[:slope], co2_r2: co2[:r2],
-      ch4_flux: ch4[:slope], ch4_r2: ch4[:r2])}
+    fluxes = %{n2o_flux: n2o[:slope], n2o_r2: n2o[:r2],
+              co2_flux: co2[:slope], co2_r2: co2[:r2],
+              ch4_flux: ch4[:slope], ch4_r2: ch4[:r2]}
+
+    {:noreply, assign(socket, round3(fluxes)) }
   end
 
   def handle_info(message, socket) do
