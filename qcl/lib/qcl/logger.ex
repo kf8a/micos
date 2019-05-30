8d0fd7a428729827a8a0b4a4fdba5ef58fc7af18
Sven Bohm <bohms@msu.edu>
2019-05-30 15:00:52 -0400

    remove extra logging

--- qcl/lib/qcl/logger.ex
+++ qcl/lib/qcl/logger.ex
@@ -2,7 +2,7 @@ defmodule Qcl.Logger do
   require Logger
 
   def save(msg) do
-       Task.start(__MODULE__, :save_to_disk, [msg])
+       # Task.start(__MODULE__, :save_to_disk, [msg])
   end
   def save_to_disk(data) do
     {:ok, table} = :dets.open_file(:qcl_data, [type: :set])
