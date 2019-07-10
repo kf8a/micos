b3c2eddd3b8d111a6bfcd7a1248cbba848388035
Sven Bohm <bohms@msu.edu>
2019-07-10 14:12:47 -0400

    spelling error

--- licor/lib/licor/reader.ex
+++ licor/lib/licor/reader.ex
@@ -25,7 +25,7 @@ defmodule Licor.Reader do
 
   def enumerate() do
     Circuits.UART.enumerate
-    |> Enum.find(fn({port, value}) -> correct_port(value) end)
+    |> Enum.find(fn({port, value}) -> correct_port?(value) end)
   end
 
   def correct_port?(%{serial_number: number}) do
