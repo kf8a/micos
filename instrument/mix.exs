2a6d97753981164e8a50bed9aa37b089dc4cba5d
sven <bohms@ideomatic.io>
2019-07-25 12:03:07 -0400

    remove amqp for now

--- instrument/mix.exs
+++ instrument/mix.exs
@@ -22,11 +22,9 @@ defmodule Instrument.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:amqp, "~> 1.2"},
+      # {:amqp, "~> 1.2"},
       {:qcl, path: "../qcl"},
       {:licor, path: "../licor"},
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
 end
