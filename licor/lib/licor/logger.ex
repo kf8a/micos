51edfa30b086d0e00209726170867d9e635bdc8f
sven <bohms@ideomatic.io>
2019-07-25 10:06:27 -0400

    pause logging licor samples

--- licor/lib/licor/logger.ex
+++ licor/lib/licor/logger.ex
@@ -2,10 +2,10 @@ defmodule Licor.Logger do
   require Logger
 
   def save(msg) do
-    Task.start(__MODULE__, :save_to_disk, [msg])
+    # Task.start(__MODULE__, :save_to_disk, [msg])
   end
 
-  def save_to_disk(data) do
+  defp save_to_disk(data) do
     {:ok, table} = :dets.open_file(:licor_data, [type: :set])
     :dets.insert_new(table, {DateTime.utc_now, data})
     :dets.close(table)
