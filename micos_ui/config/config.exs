b3066ee137f9f7468bc62c79af83d463afbf0f5e
Sven Bohm <bohms@ideomatic.io>
2020-04-21 23:43:34 +0100

    save configs

--- micos_ui/config/config.exs
+++ micos_ui/config/config.exs
@@ -31,6 +31,8 @@ config :phoenix, :json_library, Jason
 config :phoenix,
   template_engines: [leex: Phoenix.LiveView.Engine]
 
+config :instrument, :debug,  false
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
