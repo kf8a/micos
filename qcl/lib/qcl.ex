7cf6806815f3531a6b3838a9c426a299d2a13d36
sven <bohms@ideomatic.io>
2021-06-28 11:18:23 -0400

    added temperature to the qcl struct

--- qcl/lib/qcl.ex
+++ qcl/lib/qcl.ex
@@ -2,5 +2,5 @@ defmodule Qcl do
 
   defstruct instrument_datetime: DateTime.utc_now(), datetime: DateTime.utc_now(),
       ch4_ppm: 0, h2o_ppm: 0, n2o_ppm: 0, n2o_ppm_dry: 0, ch4_ppm_dry: 0,
-      n2o_ppb_dry: 0
+      n2o_ppb_dry: 0, ambient_temperature_c: 0, gas_temperature_c: 0
 end
